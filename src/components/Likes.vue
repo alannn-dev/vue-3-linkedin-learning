7cdc7286a256e749721d23324d82146b0d68afe1
Alan Boulay <boulay.alan@gmail.com>
2022-05-02 17:06:37 +0200

    Add setup fct + reference reactive

---
+++ src/components/Likes.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="id section-likes">
+        <div class="div1">{{ nbrLikes }}</div>
+        <button class="div2" @click="addLikes()">J'aime</button>
+    </div>
+</template>
+
+<script>
+import {ref} from "vue"
+export default {
+    name: "Likes",
+
+// setup function 
+    setup() { 
+        const nbrLikes = ref(7); // Reference reactive
+
+        function addLikes() {
+            nbrLikes.value++;
+        }
+        return { nbrLikes, addLikes };
+    }
+};
+</script>
+
+<style scoped>
+    .div1{
+        position: absolute;
+        top: 20%;
+        left: 10%;
+        height: 20px;
+        width: 20px;
+        background-color: red;
+        border-radius: 50% 50%;
+        z-index: 2;
+    }
+</style>
