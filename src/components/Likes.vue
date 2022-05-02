f9ce1950ec4e44f1ccc9baa3ea11cfdead2c513c
Alan Boulay <boulay.alan@gmail.com>
2022-05-02 17:38:42 +0200

    Add Likes.vue component

--- src/components/Likes.vue
+++ src/components/Likes.vue
@@ -22,15 +22,3 @@ export default {
 };
 </script>
 
-<style scoped>
-    .div1{
-        position: absolute;
-        top: 20%;
-        left: 10%;
-        height: 20px;
-        width: 20px;
-        background-color: red;
-        border-radius: 50% 50%;
-        z-index: 2;
-    }
-</style>
